7cc1ef44ff605316ea452d2df38b93c43920f12c
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-10-07 10:34:50 +0100

    add not-found page and link routing from sidenav

--- www/src/app/app-routing.module.ts
+++ www/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './_components/dashboard/dashboard.component';
+import { NotFoundComponent } from './_components/not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  { path: '404', component: NotFoundComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/404', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -5,20 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import { NavigationComponent } from './_components/navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { DashboardComponent } from './_components/dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
-import { PlayersTreeComponent } from './_components/players-tree/players-tree.component';
 import { MatTreeModule } from '@angular/material/tree';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+import { NavigationComponent } from './_components/navigation/navigation.component';
+import { DashboardComponent } from './_components/dashboard/dashboard.component';
+import { PlayersTreeComponent } from './_components/players-tree/players-tree.component';
 import { ToolbarComponent } from './_components/toolbar/toolbar.component';
+import { NotFoundComponent } from './_components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,8 @@ import { ToolbarComponent } from './_components/toolbar/toolbar.component';
     NavigationComponent,
     DashboardComponent,
     PlayersTreeComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
@@ -42,7 +46,8 @@ import { ToolbarComponent } from './_components/toolbar/toolbar.component';
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
-    MatTreeModule
+    MatTreeModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
