384aed5dca29217c00796fbe13b2582cb699f637
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-11-23 18:18:15 +0000

    Add plugin buidler-contract-sizer

--- buidler/buidler.config.js
+++ buidler/buidler.config.js
@@ -5,6 +5,8 @@ const resolve = require('path').resolve;
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 usePlugin("@nomiclabs/buidler-waffle");
+usePlugin('buidler-contract-sizer');
+
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
@@ -117,5 +119,9 @@ module.exports = {
     // This is a sample solc configuration that specifies which version of solc to use
     solc: {
         version: "0.6.8",
+    },
+    contractSizer: {
+        alphaSort: true,
+        runOnCompile: true,
     },
 };
